3ea301ef633645e9f64c0239dbc35a938cc0bbb6
Shailesh Patil <shailesh.patil@aveva.com>
2023-05-22 17:18:29 +0530

    Implementing search bar

--- src/components/Body.js
+++ src/components/Body.js
@@ -2,12 +2,25 @@ import Restaurant from "./Restaurant"
 import restaurantData from "../utility/data"
 import { useState } from "react"
 
+function filterData(input, data) {
+ return data.filter((d) => d.name.includes(input))
+}
+
 const Body = () => {
-  const [resData, setResData] = useState(restaurantData)
+  const [resData, setResData] = useState(restaurantData);
+  const [searchInput, setSearchInput] = useState();
 
   return (
     <div>
-      <div>
+      <div className="search-container">
+        <input type="text" placeholder="Search" value={searchInput} onChange={(e) => setSearchInput(e.target.value)}></input>
+        <button onClick={() =>{
+          const res = filterData(searchInput, restaurantData);
+          setResData(res);
+        } }>Search</button>
+        {
+        /* 
+        Understanding useStatehook with below example
         <button
           onClick={() => {
             filterData = resData.filter((e) => e.avgRating > 4)
@@ -15,7 +28,7 @@ const Body = () => {
           }}
         >
           Top Rated
-        </button>
+        </button> */}
       </div>
       <div className="res-list">
         {resData.map((data) => (
